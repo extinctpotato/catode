99e980e94ff84bfdeac8b48cb65ecb2d904cc856
Adam Olech <me@aole.ch>
2023-01-09 00:58:15 +0100

    Implement HnItem class

--- app/src/main/java/in/shick/diode/things/HnItem.java
+++ app/src/main/java/in/shick/diode/things/HnItem.java
@@ -1,4 +1,142 @@
 package in.shick.diode.things;
 
+import java.util.List;
+
 public class HnItem {
+    private Long id;
+    private boolean deleted;
+    private String type;
+    private String by;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBy() {
+        return by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public Long getParent() {
+        return parent;
+    }
+
+    public void setParent(Long parent) {
+        this.parent = parent;
+    }
+
+    public Long getPoll() {
+        return poll;
+    }
+
+    public void setPoll(Long poll) {
+        this.poll = poll;
+    }
+
+    public List<Integer> getKids() {
+        return kids;
+    }
+
+    public void setKids(List<Integer> kids) {
+        this.kids = kids;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Integer> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Integer> parts) {
+        this.parts = parts;
+    }
+
+    public Long getDescendants() {
+        return descendants;
+    }
+
+    public void setDescendants(Long descendants) {
+        this.descendants = descendants;
+    }
+
+    private long time;
+    private String text;
+    private boolean dead;
+    private Long parent;
+    private Long poll;
+    private List<Integer> kids;
+    private String url;
+    private Long score;
+    private String title;
+    private List<Integer> parts;
+    private Long descendants;
 }
